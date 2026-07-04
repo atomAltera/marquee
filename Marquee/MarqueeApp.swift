fe2be8f4945978e447ade410bee8470962b9fc1f
Konstantin Alikhanov <atomAltera@gmail.com>
2026-07-04 15:44:16 +0400

    Initial Commit

---
+++ Marquee/MarqueeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarqueeApp.swift
+//  Marquee
+//
+//  Created by Konstantin Alikhanov on 04/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MarqueeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
